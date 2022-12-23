8b7d856ba38aa920894b27feb9bed78f417583d7
João <72712307+jonx012@users.noreply.github.com>
2022-12-23 00:39:56 -0300

    ex-extra

---
+++ 11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 78.2
+
+console.log(`A velocidade do seu carro é ${vel}`)
+
+if (vel > 60) { // Condição Simples
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!")
+}
+
+console.log("Dirija sempre usando cinto de segurança!")
